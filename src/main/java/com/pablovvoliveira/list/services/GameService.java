624765caeac55ed84304cf9447154b0f32077201
Pablo Oliveira <pablovvoliveira@gmail.com>
2023-05-10 10:08:12 -0300

    SQL, projection, get games by list

--- src/main/java/com/pablovvoliveira/list/services/GameService.java
+++ src/main/java/com/pablovvoliveira/list/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.pablovvoliveira.list.dto.GameDTO;
 import com.pablovvoliveira.list.dto.GameMinDTO;
 import com.pablovvoliveira.list.entities.Game;
+import com.pablovvoliveira.list.projections.GameMinProjection;
 import com.pablovvoliveira.list.repositories.GameRepository;
 
 @Service
@@ -26,6 +27,12 @@ public class GameService {
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId) {
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 	
